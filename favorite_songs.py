eb484cc034928e6b3a632eaf55d90b2e531ca581
Alexandra <alexandra.chamero-rossy@hotmail.com>
2022-04-09 16:49:53 +0200

    my exercise for my favorite songs in a loop

---
+++ favorite_songs.py
@@ -0,0 +1,11 @@
+favorite_songs = ["Mathilda", 
+                  "Nothing's gonna hurt you baby", 
+                  "You are so beautiful", 
+                  "Imagine"]
+
+print("These are my favorite songs: ")
+
+for song in favorite_songs: 
+    print(song)
+
+print("What are yours?")
